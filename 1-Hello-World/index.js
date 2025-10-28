1856e859159008a2dc42f8f919640f799aa524bc
asushanthreddy9-624 <asushanthreddy9@gmail.com>
2025-10-28 11:52:03 +0000

    hello

--- 1-Hello-World/index.js
+++ 1-Hello-World/index.js
@@ -1,1 +1,7 @@
+const url="https://4geeksacademy.github.io/exercise-assets/txt/hello.txt"
 
+async function loadTextData() {
+    const response = axios.get(url);
+    console.log(response.data);
+}
+console.log(loadTextData())
